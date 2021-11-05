a1aadfeec70d22ffbb35c59459cb8dcaef63f334
congvippro <91941271+congvippro@users.noreply.github.com>
2021-11-05 22:45:27 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <math.h>
 #include <string.h>
 using namespace std;
 int main(int argc, char** argv) {
-	int a,b,c,m;
-	m=0;
+	int a,b,c,d,n;
+	int m=0;
 	cout<<"nhap chuong trinh \n";
 	cout<<"1. chuyen thap phan sang nhi phan \n";
 	cout<<"2. chuyen nhi phan sang thap phan \n";
@@ -17,13 +18,34 @@ int main(int argc, char** argv) {
 	    	    {
 	    	    	c=b%2;
 	    	    	b=b/2;
-	    	    	e[i]=c;
+	    	    	e[i]=c; 
 	    	    	m++;
 				}
 			for (int i=m;i>0;i--)
 				{
 					cout<<e[i];
 				}
+		}
+	if (a==2)
+		{
+			
+			cout<<"nhap so ";
+			cin>>b;
+			for (int i=0;b/pow(10,i)>0.1;i++)
+				{
+					m=i+1;
+				}
+			for (;m>0;m--)
+				{
+					n=pow(10,m);
+					c = b/n;
+					b = b % n ;
+					if (c==1) 
+					{
+						d=d+pow(2,m);
+					}
+				}
+		cout<<d+1;
 		}
 	return 0;
 }
